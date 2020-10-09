401f4b914114c5beac4f1ec97a056bce041a8bc9
nooralamkhansujon <fahimkhanmpi373950@gmail.com>
2020-10-09 12:23:04 -0700

    setupguide function created and its accept data whose will pass from firebase firestore database using db.collation function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+const guideList = document.querySelector('.guides');
+
+//setup guides 
+const setupGuides = (data)=>{
+    let html = '';
+//     <li>
+//     <div class="collapsible-header grey lighten-4">Guide title</div>
+//     <div class="collapsible-body white"><span>Lorem ipsum dolor sit amet.</span></div>
+//   </li>
+     data.forEach(doc=>{
+        const guide = doc.data();
+        console.log(guide);
+        html+=`<li>
+           <div class="collapsible-header grey lighten-4">${guide.title}</div>
+             <div class="collapsible-body white"><span>${guide.content}</span></div>
+           </li>`
+     });
+     guideList.innerHTML=html;
+}
+
 //setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
